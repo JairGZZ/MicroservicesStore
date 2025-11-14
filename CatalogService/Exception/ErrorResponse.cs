38cc1e75822ff91d2cce48a309ed2fb046c8ef79
JairGZZ <jairprox1234@gmail.com>
2025-11-13 21:02:38 -0500

    feat: implement custom global exception handler and error response model

---
+++ CatalogService/Exception/ErrorResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace CatalogService.Exception
+{
+
+    public class ErrorResponse
+    {
+        public string? Title { get; set; }
+
+        public string? ExceptionMessage { get; set; }
+        public string? ExceptionName { get; set; } = string.Empty;
+        public int? StatusCode { get; set; }
+
+
+
+    }
+
+}
---
+++ CatalogService/Exception/GlobalExceptionHandler.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+
+namespace CatalogService.Exception
+{
+    
+    public class GlobalExceptionHandler : IExceptionHandler
+    {
+        private readonly ILogger<GlobalExceptionHandler> _logger;
+
+        public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, System.Exception exception, CancellationToken cancellationToken)
+        {
+            _logger.LogError(exception, "Error no controlado: {Message} | Inner: {InnerMessage}",
+                exception.Message,
+                exception.InnerException?.Message);
+            var response = new ErrorResponse();
+
+            switch (exception)
+            {
+                case NotImplementedException:
+                    response.Title = "Algo Salio Mal";
+                    response.StatusCode = StatusCodes.Status500InternalServerError;
+                    response.ExceptionMessage = exception.Message;
+                    break;
+                case UnauthorizedAccessException:
+                    response.Title = "No esta Autorizado";
+                    response.StatusCode = StatusCodes.Status401Unauthorized;
+                    response.ExceptionMessage = exception.Message;
+                    break;
+                   
+                default :
+                    response.Title = "Error del Servidor";
+                    response.StatusCode = StatusCodes.Status500InternalServerError;
+                    response.ExceptionMessage = "Ocurrio un error inesperado";
+                    response.ExceptionName = exception.GetType().Name;
+                    break;
+
+            }
+          
+
+            await httpContext.Response.WriteAsJsonAsync(response,cancellationToken);
+
+
+            return true;
+
+        }
+    }
+}
