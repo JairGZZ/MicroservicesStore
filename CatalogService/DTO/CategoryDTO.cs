867e1f4a88275a3ef1a693ed098f35a6cf14b33e
JairGZZ <jairprox1234@gmail.com>
2025-11-13 21:02:00 -0500

    feat: add CategoryDTO and refactor ProductResponse to use properties

---
+++ CatalogService/DTO/CategoryDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace CatalogService.DTO
+{
+    public class CategoryDTO
+    {
+        public string Name { get; set; } = null!;
+    }
+}
--- CatalogService/DTO/ProductResponse.cs
+++ CatalogService/DTO/ProductResponse.cs
@@ -2,13 +2,14 @@
 
 namespace CatalogService.DTO
 {
-    public record ProductResponse(
-        
-        int productid,
-        string name,
-        string description,
-        decimal price,
-        int stock
+    public record  ProductResponse
+    {
 
-        );
+        public int productid { get; init; }
+        public string name { get; init; }
+        public string description { get; init; }
+        public decimal price { get; init; }
+        public int stock { get; init; }
+
+    } 
 }
