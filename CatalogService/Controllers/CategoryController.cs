928a088304dae4173c3b8d92bccb2a21f6564332
JairGZZ <jairprox1234@gmail.com>
2025-11-13 21:01:43 -0500

    feat: add Category and Product controllers with CRUD operations

---
+++ CatalogService/Controllers/CategoryController.cs
@@ -0,0 +1,47 @@
+﻿using CatalogService.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService; 
+        }
+        [HttpPost("create")]
+        public async Task<ActionResult<DTO.CategoryDTO>> CreateCategoryAsync([FromBody] DTO.CategoryDTO category)
+        {
+            var createdCategory = await _categoryService.CreateCategoryAsync(category);
+            return Ok(createdCategory);
+        }
+        [HttpPost("delete")]
+        public async Task<IActionResult> DeleteAsync(int categoryId)
+        {
+            await _categoryService.DeleteAsync(categoryId);
+            return NoContent();
+        }
+        [HttpGet("getAll")]
+        public async Task<ActionResult<IEnumerable<DTO.CategoryDTO>>> GetAllCategories()
+        {
+            var categories = await _categoryService.GetAllCategories();
+            return Ok(categories);
+        }
+        [HttpGet("findByid")]
+        public async Task<ActionResult<DTO.CategoryDTO>> FindById(int categoryId)
+        {
+            var category = await _categoryService.FindById(categoryId);
+            return Ok(category);
+        }
+        [HttpPut("update")]
+        public async Task<ActionResult<DTO.CategoryDTO>> UpdateCategory(int categoryId, [FromBody] DTO.CategoryDTO update)
+        {
+            var updatedCategory = await _categoryService.UpdateCategory(categoryId, update);
+            return Ok(updatedCategory);
+        }
+    }
+}
--- CatalogService/Controllers/ProductController.cs
+++ CatalogService/Controllers/ProductController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CatalogService.DTO;
+using CatalogService.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CatalogService.Controllers
@@ -7,5 +9,29 @@ namespace CatalogService.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        private readonly IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpPost("create")]
+        public async Task<ActionResult<CreateProductDto>> CreateProductAsync([FromBody] DTO.CreateProductDto product)
+        {
+            var createdProduct = await _productService.CreateProductAsync(product);
+            return Ok(createdProduct);
+        }
+        [HttpPost("delete")]
+
+        public async Task<IActionResult> DeleteAsync(int productId)
+        {
+            await _productService.DeleteAsync(productId);
+            return NoContent();
+        }
+        [HttpGet("getAll")]
+        public async Task<ActionResult<IEnumerable<ProductResponse>>> GetAllProducts()
+        {
+            var products = await _productService.GetAllProducts();
+            return Ok(products);
+        }
     }
 }
